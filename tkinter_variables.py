c044d4598f68583e73b424c501811af4d3369087
Dann . <zadanfaidelta@gmail.com>
2024-07-03 15:24:08 +0700

    Trying tkinter string variables

---
+++ tkinter_variables.py
@@ -0,0 +1,24 @@
+
+# Here i wanna try to make the Label should always have the same text as Entry
+# Im using stringvar to make them automatically updated
+
+import tkinter as tk
+from tkinter import ttk
+
+# window
+app = tk.Tk()
+app.title = 'Tkinter Variables'
+app.geometry('500x300')
+
+# tkinter variables
+string_var = tk.StringVar()
+
+# widgets
+label = ttk.Label(master = app, text = 'Label', textvariable = string_var)
+label.pack()
+
+entry = ttk.Entry(master = app, textvariable = string_var)
+entry.pack()
+
+# run
+app.mainloop()
