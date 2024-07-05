94d8e2397b988aaa43ca78dd3e034f397369a0f0
Dann . <zadanfaidelta@gmail.com>
2024-07-05 21:53:25 +0700

    Adding comment on events section

--- 6_events.py
+++ 6_events.py
@@ -16,6 +16,15 @@ entry.pack()
 btn = ttk.Button(app, text = 'A Button')
 btn.pack()
 
+# Events can be lots of things, such as..
+# (1) Keyboard inputs
+# (2) Widgets getting changed
+# (3) Widgets gets selected / unselected
+# (4) Mouse click / motion / wheel. and so on
+
+# I need to bind events to a widget --> Widget.bind(event, function)
+# Format : <modifier-type-detail>
+
 # events
 app.bind('<Alt-KeyPress-a>', lambda event: print(event))
 
