4455b22dedb2e8a42ed229adfc58e4541d3bd6bc
Dann . <zadanfaidelta@gmail.com>
2024-07-08 21:29:16 +0700

    Trying combobox function, events & labels combination

--- 7_combo&spinbox.py
+++ 7_combo&spinbox.py
@@ -10,10 +10,20 @@ app.geometry('500x300')
 # combobox
 
 items = ('Ice cream', 'Pizza', 'Broccoli')
-combo = ttk.Combobox(app)
+food_string = tk.StringVar(value = items[0]) # To set value for the first place
+combo = ttk.Combobox(app, textvariable= food_string)
 combo['values'] = items
+# combo.configure(values = items)
 combo.pack()
 
+# events for combobox
+
+combo.bind('<<ComboboxSelected>>', lambda event: combo_label.config(text = f'Selected value: {food_string.get()}'))
+
+# combobox label
+
+combo_label = ttk.Label(app, text = 'a label')
+combo_label.pack()
 
 # Both need to get a list of values
 # Both can be connected to a Tkinter variables
