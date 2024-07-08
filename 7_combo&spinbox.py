fc3d32bc473303a0decbdc7234958f13139231e5
Dann . <zadanfaidelta@gmail.com>
2024-07-08 21:38:10 +0700

    playing with spinbox and all the events, functions, and variables

--- 7_combo&spinbox.py
+++ 7_combo&spinbox.py
@@ -7,7 +7,7 @@ app = tk.Tk()
 app.title('Combobox & Spinbox')
 app.geometry('500x300')
 
-# combobox
+# Combobox
 
 items = ('Ice cream', 'Pizza', 'Broccoli')
 food_string = tk.StringVar(value = items[0]) # To set value for the first place
@@ -24,6 +24,24 @@ combo.bind('<<ComboboxSelected>>', lambda event: combo_label.config(text = f'Sel
 
 combo_label = ttk.Label(app, text = 'a label')
 combo_label.pack()
+
+
+# Spinbox
+
+spin_int = tk.IntVar(value = 12)
+spin = ttk.Spinbox(
+    app, 
+    from_ = 3, 
+    to = 20, 
+    increment = 3, 
+    command = lambda: print(spin_int.get()),
+    textvariable = spin_int
+) 
+# increment = jump values, from_ & to = list of values to spin
+spin.bind('<<Increment>>', lambda event: print('up'))
+spin.bind('<<Decrement>>', lambda event: print('down'))
+# spin['value'] = (1,2,3,4,5) 
+spin.pack()
 
 # Both need to get a list of values
 # Both can be connected to a Tkinter variables
